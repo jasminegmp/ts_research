aacfee46419e170df0f0821625ffb4d8a0461735
hyun1e <jasmine.kim.18@gmail.com>
2017-12-05 00:56:42 -0800

    Cleaner TS generator

--- ts_generator.m
+++ ts_generator.m
@@ -1,23 +1,21 @@
-function [t, y] = ts_generator(Fs, L, noise)
+function [x, y] = ts_generator(Fs, L, noise)
     
-    T = 1/Fs;                   % Sample time
-    t = (0:L-1)*T;              % Time vector
-
+    %T = 1/Fs;                   % Sample time
+    %T = Fs
+    %t = (0:L-1)*T;              % Time vector
+    ts = Fs;
+    x = 0:ts:L;
+    
     %% Random data with piecewise functions
-    for k = 1 : length(t)
-        if k < (length(t)/3)
-            y(k) = 2*sin(t(k)) + (randn(size(k))/noise);
-        % Removing noisy section for now
-        %elseif k >= (length(t)/3) && k < length(t)/2
-        %    y(k) = (randn(size(100)));
-        elseif k >= (length(t)/3) && k < length(t)/2
-            y(k) = 3*abs(sin(t(k))/2) + (randn(size(k))/(noise*2));
-        else k < 3*length(t)/4
-        %else if k < 3*length(t)/4
-            y(k) = 3*sin(3*t(k)) + (randn(size(k))/noise);
-        %else
-        %    y(k) = (randn(size(100)));    
+    for k = 1:length(x)
+        if x(k) < L/4
+            y(k) = sin(x(k)) + (randn(size(k))/noise);
+        elseif x(k) >= L/4 && x(k) < L*5/8
+            y(k) = abs(sin(x(k))) + (randn(size(k))/noise);
+        elseif x(k) > L*.64 && x(k) < L*3/4
+            y(k) = abs(sawtooth(x(k))) + (randn(size(k))/noise); 
+        else  
+            y(k) = (randn(size(k))/noise);
         end
-
     end
 end
