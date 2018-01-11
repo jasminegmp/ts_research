7d545db3244da12276693efb788c76f21be08a99
Jasmine Kim <jasmine.kim.18@gmail.com>
2018-01-11 14:31:50 -0800

    Finds top 10% minimums in MP

--- ts_summarizer_v4.m
+++ ts_summarizer_v4.m
@@ -11,7 +11,7 @@ period = 2*pi; % Assume for now, I've already derived period from FFT
 Fs = 1/period;                    % Sampling frequency
 L = period*segments*10;                   % Length of signal
 noise = 1;
-
+ 
 %% Generate ts
 [t,y] = ts_generator(Fs, L, noise);
 
@@ -31,3 +31,14 @@ n = round(MAGIC_percentage*length(y));
 [mp_y,mp_t] = mink(matrixProfile, n, 1);
 
 %% Go through minimums looking for first dips that are within MP segment length
+%  These minimum values are stored in target_dp
+target_dp = [];
+diff_mp_t = diff(mp_t);
+for idx = 1:diff_mp_t
+    if abs(diff_mp_t(idx)) <= MAGIC_mp_seg_len
+        target_dp = [target_dp, idx];
+    end
+end
+
+%% 
+dp = findNN(y,y(target_dp(1):target_dp(1)+MAGIC_mp_seg_len));
