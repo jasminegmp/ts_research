ab0e9b6ceb537a32ba50f336ebcd9a5d7f72a48e
Jasmine Kim <jasmine.kim.18@gmail.com>
2018-04-24 11:59:49 -0700

    Working

--- 012418/new_sum_fastMPdist.m
+++ 012418/new_sum_fastMPdist.m
@@ -56,7 +56,7 @@ plot(ts_2);
 title('Time series and Segment');
 count = 1;
 %% Divide up time series by segment length and claculate distance array
-for idx = 1:seg_len:tot_len - seg_len*2
+for idx = 1:seg_len:tot_len - seg_len
     
     seg_0 = idx;
     seg_1 = idx + seg_len - 1;
@@ -89,6 +89,11 @@ merge_history{merge_count,1} = dist_mat;
 % 3. Updates distance matrix
 % 4. Repeat until all time series segments have been merged
 while (size(dist_mat,1) > 1)
+    if any(~isnan(cell2mat(dist_mat(:,1)))) == 0
+        break;
+    end
+
+    last_element = 0;
     
     % 1.
     [min_val, min_idx] = min(cell2mat(dist_mat(:,1)));
@@ -114,32 +119,70 @@ while (size(dist_mat,1) > 1)
     hold on;
     plot(ts_1, 'LineWidth', 0.7);
     plot(loc_0:loc_1, m_seg_0, 'Color', [1 0 0], 'LineWidth', 0.7)
-    plot(loc_2:loc_3, m_seg_1, 'Color', [0.5 0.5 0.5], 'LineWidth', 0.7)
-
+    plot(loc_2:loc_3, m_seg_1, 'Color', [0.1 0.1 0.1], 'LineWidth', 0.7)
     
+    %plot old
+    if merge_count > 1
+        for i = 1:size(merged_data,1)
+            temp_loc_0 = cell2mat(merged_data{i, 1});
+            temp_loc_1 = cell2mat(merged_data{i, 2});
+            temp_seg = cell2mat(merged_data{i, 3});
+            plot(temp_loc_0:temp_loc_1, temp_seg, 'Color', [0.5 0.5 0.5], 'LineWidth', 0.7)
+        end
+    end
+% 
+%     for i = 1:length(merge_history{merge_count,1})
+%         if ~isnan(merge_history{merge_count,1}{i,8})
+%             plot(loc_2:loc_3, m_seg_1, 'Color', [0.5 0.5 0.5], 'LineWidth', 0.7)
+%         end
+%     end
     
 
     
     %do merge
-    next_loc_0 = cell2mat(dist_mat(min_idx+1,4));
-    next_loc_1 = cell2mat(dist_mat(min_idx+1,5));
-    new_m_seg_1 = ts_1(next_loc_0:next_loc_1);
+    if min_idx < length(dist_mat)
+        % find next valid value
+        for j = min_idx+1:length(dist_mat)
+            if ~isnan(cell2mat(dist_mat(j,1)))
+                next_loc_0 = cell2mat(dist_mat(j,4));
+                next_loc_1 = cell2mat(dist_mat(j,5));
+                new_m_seg_1 = ts_1(next_loc_0:next_loc_1);
+                merge_loc = j;
+                if min_idx+1 == j
+                    last_element = 0;
+                else
+                    dist_mat(j,2) = dist_mat(min_idx,2);
+                    dist_mat(j,3) = dist_mat(min_idx,3);
+                    last_element = 0;
+                end
+                break;
+            else
+                last_element = 1;
+            end
+        end
+        % if only nan left
+    end
     
     % Remove merged segment
-    % ts_1(loc_0:loc_1) stays the same for left merge
-    ts_1(loc_2:loc_3) = NaN;
+
     merged_data{merge_count,1} = {loc_2};
     merged_data{merge_count,2} = {loc_3};
     merged_data{merge_count,3} = {ts_1(loc_2:loc_3)};
+    % ts_1(loc_0:loc_1) stays the same for left merge
+    ts_1(loc_2:loc_3) = NaN;
     dist_mat(min_idx,:) = {NaN};
     dist_mat{min_idx,8} = merge_count;
     
+    if min_idx < length(dist_mat) && (last_element == 0)
     %3. calculate new distance and location
-    dist = fastMPdist_SS(m_seg_0,new_m_seg_1,fastMPdist_seg_len);
-    dist_mat(min_idx + 1,1) = {dist};
-    dist_mat(min_idx + 1,2) = {loc_0};
-    dist_mat(min_idx + 1,3) = {loc_1};
-
+        % remove all NaN and replace with 0
+        m_seg_0(isnan(m_seg_0)) = 0
+        m_seg_1(isnan(m_seg_1)) = 0
+        dist = fastMPdist_SS(m_seg_0,new_m_seg_1,fastMPdist_seg_len);
+        dist_mat(merge_loc,1) = {dist};
+        dist_mat(merge_loc,2) = {loc_0};
+        dist_mat(merge_loc,3) = {loc_1};
+    end
     merge_count = merge_count + 1;
 
     
@@ -163,27 +206,27 @@ end
 % level = level + 1;
 
 % Go plot output
-for j = 1:length(final_dist)
-    figure;
-    title(j);
-    hold on;
-    plot(ts,'Color', [.1 .4 1], 'LineWidth', 0.7);
-    
-    for k = 1:1:length(final_dist)
-        if cell2mat(final_dist(k,7)) <= j
-            if cell2mat(final_dist(k,7)) < j
-                plot(cell2mat(final_dist(k,3)):cell2mat(final_dist(k,4)), cell2mat(final_dist(k,6)), 'Color', [0.5 0.5 0.5], 'LineWidth', 0.7)
-                plot(cell2mat(final_dist(k,2)):cell2mat(final_dist(k,3)), cell2mat(final_dist(k,5)), 'Color', [0.5 0.5 0.5], 'LineWidth', 0.7)
-            else
-                plot(cell2mat(final_dist(k,2)):cell2mat(final_dist(k,3)), cell2mat(final_dist(k,5)), 'Color', [1 0 0], 'LineWidth', 0.7)
-                plot(cell2mat(final_dist(k,3)):cell2mat(final_dist(k,4)), cell2mat(final_dist(k,6)), 'Color', [0.5 0.5 0.5], 'LineWidth', 0.7)
-            end
-        end
-    end
-    
-    %plot(loc_1:loc_2, m_seg_1, 'Color', [0 0 1], 'LineWidth', 0.7)
-end
-    
+% for j = 1:length(final_dist)
+%     figure;
+%     title(j);
+%     hold on;
+%     plot(ts,'Color', [.1 .4 1], 'LineWidth', 0.7);
+%     
+%     for k = 1:1:length(final_dist)
+%         if cell2mat(final_dist(k,7)) <= j
+%             if cell2mat(final_dist(k,7)) < j
+%                 plot(cell2mat(final_dist(k,3)):cell2mat(final_dist(k,4)), cell2mat(final_dist(k,6)), 'Color', [0.5 0.5 0.5], 'LineWidth', 0.7)
+%                 plot(cell2mat(final_dist(k,2)):cell2mat(final_dist(k,3)), cell2mat(final_dist(k,5)), 'Color', [0.5 0.5 0.5], 'LineWidth', 0.7)
+%             else
+%                 plot(cell2mat(final_dist(k,2)):cell2mat(final_dist(k,3)), cell2mat(final_dist(k,5)), 'Color', [1 0 0], 'LineWidth', 0.7)
+%                 plot(cell2mat(final_dist(k,3)):cell2mat(final_dist(k,4)), cell2mat(final_dist(k,6)), 'Color', [0.5 0.5 0.5], 'LineWidth', 0.7)
+%             end
+%         end
+%     end
+%     
+%     %plot(loc_1:loc_2, m_seg_1, 'Color', [0 0 1], 'LineWidth', 0.7)
+% end
+%     
 
 
 % Fill one node
